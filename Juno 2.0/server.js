1a2d0dd32deb4b8a163c9d21330aa7e6d9ffd951
dicedmango <alex.richardson-@live.co.uk>
2016-12-11 17:47:13 +0000

    updates

---
+++ Juno 2.0/server.js
@@ -0,0 +1,19 @@
+const PORT=9904; 
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('./'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+server = app.listen(PORT);
+
+var host = server.address().address
+var port = server.address().port
+
+console.log("server is listening on port %s", port);
